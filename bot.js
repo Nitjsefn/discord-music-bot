f9322262070d26005e95ebb0b1c8db715fa75f52
Nitjsefn <28672240+Nitjsefn@users.noreply.github.com>
2021-12-08 20:26:21 +0100

    deleted commented code which will be not useful anymore

--- bot.js
+++ bot.js
@@ -1,6 +1,3 @@
-//var json_file = require("./bot.json");
-//const token = json_file.token;
-//const prefix = json_file.prefix;
 const {prefix, token} = require("./bot.json");
 const { Client, Intents } = require('discord.js');
 console.log(prefix, token);
