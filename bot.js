94baea76785fb15d5ef0782f3ffaacbec52c0c81
Nitjsefn <28672240+Nitjsefn@users.noreply.github.com>
2021-12-09 13:11:09 +0100

    created bot object, added intents to GUILD and GUILD_MESSAGES, added method for 'ready' event and simple filtration for coming messages

--- bot.js
+++ bot.js
@@ -1,3 +1,12 @@
 const {prefix, token} = require("./bot.json");
 const { Client, Intents } = require('discord.js');
-console.log(prefix, token);
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { log } = console;
+bot.login(token);
+bot.once("ready", () => log("Bot connected"));
+bot.on("messageCreate", msg => {
+	log("Got message")
+	if(msg.author.isBot) return;
+	if(msg.content[0] != prefix) return;
+	log("Got command!");
+});
