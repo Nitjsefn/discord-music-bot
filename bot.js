804627ae89b16c67812b59ccf69fb5564a06f4f5
Nitjsefn <8672240+Nitjsefn@users.noreply.github.com>
2021-12-17 12:03:46 +0100

    changed importing @discordjs/voice functions to importing whole @discordjs.voice into DCVoice, added function which make bot leaves voice channel

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const {prefix, token} = require("./bot.json");
 const { Client, Intents } = require('discord.js');
 const { resourceUsage, listenerCount } = require("process");
 const { resourceLimits } = require("worker_threads");
-const { joinVoiceChannel } = require('@discordjs/voice');
+const DCVoice = require('@discordjs/voice');
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const { log } = console;
 var queuesInGuildsCollection = new Map();
@@ -48,7 +48,13 @@ function messageCreateAndUpdateMethod(msg)
 		case "np": displayNowPlayingSong(msg); break;
 		case "queue": displayQueue(msg); break;
 		case "search": searchForMusic(msg, arguments); break;
-		case "join": joinVoiceChannel({channelId: msg.member.voice.channelId, guildId: msg.guildId, adapterCreator: msg.channel.guild.voiceAdapterCreator}); break;
+		case "join": DCVoice.joinVoiceChannel({channelId: msg.member.voice.channelId, guildId: msg.guildId, adapterCreator: msg.channel.guild.voiceAdapterCreator}); break;
 		default: msg.reply("Wrong command"); break;
 	}
+}
+
+function leaveVoiceChannel(msg)
+{
+	let connection = DCVoice.getVoiceConnection(msg.guildId);
+	connection.destroy();
 }
