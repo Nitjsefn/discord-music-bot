1ea505b3f22aa45431d513380d85a07abb4e5b1f
Nitjsefn <8672240+Nitjsefn@users.noreply.github.com>
2021-12-18 22:58:03 +0100

    separated pl command into 2 different command - pl and pld, added saving all mp3 files from specified directory into collection with guildID inside of pld command

--- bot.js
+++ bot.js
@@ -5,8 +5,10 @@ const { resourceLimits } = require("worker_threads");
 const DCVoice = require('@discordjs/voice');
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 const { log } = console;
+const fs = require('fs');
 var queuesInGuildsCollection = new Map();
-class Song
+var pathToPlaylistsLibrary = new Map();
+/*class Song
 {
 	Song()
 	{
@@ -15,7 +17,7 @@ class Song
 		let durationInSec = 0;
 		let author = "";
 	}
-}
+}*/
 
 bot.login(token);
 bot.once("ready", () => log("Bot connected"));
@@ -32,6 +34,7 @@ function messageCreateAndUpdateMethod(msg)
 	switch(command)
 	{
 		case "pl": playLocal(msg, arguments); break;
+		case "pld": playLocalPlaylist(msg, arguments); break;
 		case "skip": skipMusic(msg); break;
 		case "pause": pauseMusic(msg); break;
 		case "stop": stopMusic(msg); break;
@@ -42,6 +45,7 @@ function messageCreateAndUpdateMethod(msg)
 		case "queue": displayQueue(msg); break;
 		case "search": searchForMusic(msg, arguments); break;
 		case "join": DCVoice.joinVoiceChannel({channelId: msg.member.voice.channelId, guildId: msg.guildId, adapterCreator: msg.channel.guild.voiceAdapterCreator}); break;
+		case "setPath": pathToPlaylistsLibrary.set(msg.guildId, arguments[0]); break;
 		default: msg.reply("Wrong command"); break;
 	}
 }
@@ -50,4 +54,43 @@ function leaveVoiceChannel(msg)
 {
 	let connection = DCVoice.getVoiceConnection(msg.guildId);
 	connection.destroy();
+}
+
+function playLocalPlaylist(msg, args)
+{
+	let songsList;
+	let guildID = msg.guildId;
+	let playlistName = arguments[0];
+	if(pathToPlaylistsLibrary.hasValue(guildID))
+	{
+		let defPath = pathToPlaylistsLibrary.get(guildID);
+		if (!(defPath[defPath.length - 1] === '/')) defPath += '/';
+		let pathToLocalPlaylist = defPath + playlistName;
+		let songsList = new Array();
+		fs.readdir(pathToLocalPlaylist, (err, files) => 
+		{
+			if (err) { console.log(err); return; }
+			if (queuesInGuildsCollection.hasValue(guildID)) songsList = queuesInGuildsCollection.get(guildID);
+			foreach (file in files)
+			{
+				if(file.indexOf("mp3") < 0) continue;
+				songsList.add(file);
+			}
+			queuesInGuildsCollection.set(guildID, songsList);
+		});
+	}
+	else
+	{
+		fs.readdir(playlistName, (err, files) => 
+		{
+			if (err) { console.log(err); return; }
+			if (queuesInGuildsCollection.hasValue(guildID)) songsList = queuesInGuildsCollection.get(guildID);
+			foreach (file in files)
+			{
+				if(file.indexOf("mp3") < 0) continue;
+				songsList.add(file);
+			}
+			queuesInGuildsCollection.set(guildID, songsList);
+		});
+	}
 }
